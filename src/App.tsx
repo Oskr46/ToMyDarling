83cd750f852211cbc2f12da79845b4585ae205fd
Oskr46 <oscarraf64@gmail.com>
2025-09-29 08:21:03 -0400

    Dia 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 //import { useState } from 'react';
-import DailyGif from "../public/darling/PUCCAGif.gif"
+import DailyGif from "../public/darling/dailyGif.gif"
 import './App.css'
 
 function App() {
@@ -55,28 +55,86 @@ function App() {
       <div className='content'>
         <div className='hiText'>
           <h3>
-            Hola mi amor, buenos días. Espero que pases un bueeen día.
+            ♥ Hola Serenita linda ♥, espero que hayas dormido bieen. Te amo Corazón, que tengas un buen día.
           </h3>
 
           <p>
-            Puees, bueno, no tenía pensado aún que vieras todo esto, pero bueno, soy malisimo mintiendo y ocultando cosas xd
-            aún no sé bien que poner aquí, te amo muchisimo corazón. 
+            Cada día me enamoro más de ti, amor mío, me gustas muchísimo. Te amo como no tienes idea. De verdad que si no estás
+            me aburro muchísimo, ya no paro de pensar en tí todo el día.
           </p>
 
           <p>
-            Puees, es inevitable el hecho de esconder que me gustabas en realidad desde el principio, pero bueno, sabes
-            como soy, y quería en cierto modo ponerte a prueba, porque bueno, me daba mucho miedo volver a
-            quedarme con el amor en las manos. Pero bueno, poco a poco me fuí enamorando muchisimo de tí
-            (más que antes xd), y pues, digamos que superaste las expectativas y eres todo lo que buscaba, basicamente,
-            estoy perdidamente loco por ti, princesa mía.
+            Estaba escuchando música y bueno, tengo varias que me recuerdan a tí. Espero que te gusten amor mío, así como tu a mí ♥
           </p>
+          <ul>
+          <li>
+            <h4>
+              Bonita - Cabas. <br></br> <a href="https://youtu.be/zLlmvol7g4k?si=qW-kCaH1v6NEI6CT">Presiona aquí, Princesa ♥</a>
+            </h4>
+          </li>
+      <br></br>
+          <li>
+            <h4>
+              Amo Las Estrellas - Zapato3 <br></br> <a href="https://youtu.be/Pd79SSm0X1Y?si=M7Ou4v9dRsjcDHb1">Presiona aquí, Princesa ♥</a>
+            </h4>
+          </li>
+      <br></br>
+          <li>
+            <h4>
+              La Que Me Gusta - Los Amigos Invisibles <br></br> <a href="https://youtu.be/rigubwkGxdw?si=z49NxHXF4D16RfEx">Presiona aquí, Princesa ♥</a>
+            </h4>
+          </li>
+      <br></br>
+          <li>
+            <h4>
+              Flamingo - La Vida Boheme <br></br> <a href="https://youtu.be/xoWRkd3oGcs?si=Uk5APLfjAev2Yk_C">Presiona aquí, Princesa ♥</a>
+            </h4>
+          </li>
+      <br></br>
+          <li>
+            <h4>
+            Caraluna - Bacilos <br></br> <a href="https://youtu.be/D3JKXJpPhM0?si=OpMx7cxlfdA9v_Uw">Presiona aquí, Princesa ♥</a>
+            </h4>
+          </li>
+      <br></br>
+          <li>
+            <h4>
+              De mi, de mi, de mi - Lasso <br></br> <a href="https://youtu.be/Q7eLPn9J-2o?si=ZMbYxCrAgwpywCq0">Presiona aquí, Princesa ♥</a>
+            </h4>
+          </li>
+      <br></br>
+          <li>
+            <h4>
+              Te veo - Lasso <br></br> <a href="https://youtu.be/Vqq6tO3Xeko?si=1kQWPyzaf16Abq7P">Presiona aquí, Princesa ♥</a>
+            </h4>
+          </li>
+      <br></br>
+          <li>
+            <h4>
+              Obsesionario en La Mayor - Tan Bionica <br></br> <a href="https://youtu.be/V8hIlJt0lDY?si=x1RbjM07_54abWn3">Presiona aquí, Princesa ♥</a>
+            </h4>
+          </li>
+      <br></br>
+          <li>         
+            <h4>
+              Loca - Tan Bionica <br></br> <a href="https://youtu.be/rehqofVh7-Y?si=zLTHfafRegbfzBgd">Presiona aquí, Princesa ♥</a>
+            </h4>
+          </li>
+      <br></br>
+          <li>
+            <h4>
+              Tu Ternura - Gualberto Ibarreto <br></br> <a href="https://youtu.be/EAB1hCmxq4U?si=XA4KY3Fz8psUIP14">Presiona aquí, Princesa ♥</a>
+            </h4>
+          </li>
+      <br></br>   
+          </ul>
 
           <h3>
-            Te amo muchisimo.
+            Espero que te gusten princesa linda. Te amo ♥
           </h3>
-          <h3>♥Y que pases un bonito día.♥</h3>
+          <h3>♥ Que pases un bonito día. ♥</h3>
           
-          <div className='image-container' data-caption="Pucca ♥">
+          <div className='image-container' data-caption="♥♥♥">
             <img src={DailyGif} width={180} height={180} alt="Mi amor precioso" />
           </div>
 
